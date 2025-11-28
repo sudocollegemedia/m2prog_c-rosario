19f7f3a907c132fe75be8580aad1d8bd5bfa9d4a
Rosario <40689@ma-web.nl>
2025-11-28 14:28:23 +0100

    Add RijSchool and nested classes

---
+++ RijSchool.cs
@@ -0,0 +1,34 @@
+public class RijSchool
+{
+    public class Auto
+    {
+    }
+
+    public class RijLeraar
+    {
+    }
+
+    public class Student
+    {
+    }
+
+    public class LesPakket
+    {
+    }
+
+    public class RijTest
+    {
+    }
+
+    public class TheorieTest
+    {
+    }
+
+    public class Dag
+    {
+    }
+
+    public class LesUur
+    {
+    }
+}
