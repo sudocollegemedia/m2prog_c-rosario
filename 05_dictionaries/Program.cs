5ce6d6975c940ae79fc84ebd0fa65540ab3670a6
Rosario <40689@ma-web.nl>
2026-01-23 14:16:51 +0100

    05_dictionaries

---
+++ 05_dictionaries/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _05_dictionaries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, string> personeelOpNummer = new Dictionary<int, string>();
+        personeelOpNummer.Add(1, "Jan Jansen");
+        personeelOpNummer.Add(2, "Piet Pietersen");
+        Console.WriteLine(personeelOpNummer[1]);
+
+        foreach (KeyValuePair<int, string> medewerker in personeelOpNummer)
+        {
+            Console.WriteLine($"Nummer: {medewerker.Key}, Naam: {medewerker.Value}");
+        }
+    }
+}
